58662ce20145b72065406f1e67f2672d75a3fb40
shaheerhashim10 <shaheerhashim10@gmail.com>
2021-09-24 01:26:42 +0500

    Structure

---
+++ src/components/DisplayArticles.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/DisplayArticles.css'
+import axios from 'axios';
+
+function DisplayArticles() {
+    // state = {
+    //     articles: []
+    // }
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default DisplayArticles
